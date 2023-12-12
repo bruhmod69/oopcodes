27452460d716db48972d7ed25de014d76266dbda
hgfhgf <153413164+bruhmod69@users.noreply.github.com>
2023-12-12 21:45:22 +0530

    Update Assignment7code.cpp

--- SYCOB30OOP/Assignment7/Assignment7code.cpp
+++ SYCOB30OOP/Assignment7/Assignment7code.cpp
@@ -2,6 +2,21 @@
 #include<map>
 #include<string>
 using namespace std;
+int main(){
+map<string,int>pdb;
+pdb["mh"]=628;
+pdb["up"]=7318;
+pdb ["mp"]=765;
+string name;
+cout<<"enter the name of the state to be searched"<<endl;
+cin>>name;
+cout<<"the population of the state "<<name<<" is "<<pdb[name]<<endl;
+return 0;
+}
+/*#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
 int main()
 {
 map<string,int>populationMap;
@@ -26,4 +41,4 @@ else
 cout<<"Key is not in populationMap";
 populationMap.clear();
 return 0;
-}
+}*/
